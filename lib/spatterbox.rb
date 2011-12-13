4669d219d8007abc64e74be2cdedc30f1d9c26e2
Brent Vatne <brentvatne@gmail.com>
2011-12-12 23:31:26 -0500

    Rename shape_calculator.rb to shapely.rb

--- lib/spatterbox.rb
+++ lib/spatterbox.rb
@@ -2,4 +2,4 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'spatterbox/tag_prettifier.rb'
 require 'spatterbox/recursive_tagger.rb'
-require 'spatterbox/shape_calculator.rb'
+require 'spatterbox/shapely.rb'
